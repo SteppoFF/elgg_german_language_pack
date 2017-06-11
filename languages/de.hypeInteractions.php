e2bd5af4ee74927af20a75cab05e5ee2d013cac4
SteppoFF <steppo@sehr-chaos-company.de>
2017-06-11 12:22:36 +0200

    Create de.hypeInteractions.php

---
+++ languages/de.hypeInteractions.php
@@ -0,0 +1,74 @@
+<?php
+/*
+ * Translation for hypeInteractions
+ * @package hypeInteractions
+ * @subpackage Languages.German
+ */
+$de_extend = array(
+  'interactions:comment:subject' => 'Kommentar von %s',
+  'interactions:comment:body' => '%s<span>: </span>%s',
+  'interactions:attachments:labelled' => 'Anhänge:',
+  'interactions:response:email:subject' => '%s %s %s',
+  'interactions:river_object:title_subject' => 'Aktivität von %s',
+  'interactions:river_object:title' => 'Aktivität',
+  'interactions:likes:no_results' => 'Dieser Beitrag gefällt noch niemandem',
+  'interactions:comments:no_results' => 'Es gibt noch keine Kommentare',
+  'interactions:like:object:default' => '%s gefällt %s',
+  'interactions:user:default' => 'Benutzer',
+  'interactions:group:default' => 'Gruppe',
+  'interactions:object:file' => 'Datei',
+  'interactions:object:page' => 'Seite',
+  'interactions:object:page_top' => 'Seite',
+  'interactions:object:comment' => 'Kommentar',
+  'interactions:action:discussion_reply:on:object:discussion' => 'geantwortet auf',
+  'interactions:action:comment:on:object:discussion_reply' => 'geantwortet auf',
+  'interactions:action:comment:on:object:comment' => 'geantwortet auf',
+  'interactions:action:comment:on:object' => 'kommentiert auf',
+  'interactions:comment' => 'Kommentar',
+  'interactions:likes:notifications:subject' => '%s gefällt %s',
+  'interactions:likes:before' => 'Gefällt',
+  'interactions:likes:after' => 'Gefällt nicht mehr',
+  'interactions:reply:create' => 'Antworten',
+  'interactions:comment:create' => 'Kommentieren',
+  'interactions:comments:edit:title' => 'Kommentar bearbeiten',
+  'interactions:likes:title' => 'Leuten denen %s gefällt',
+  'hypeInteractions:usersettings:title' => 'Kommentare',
+  'interactions:comments:title' => 'Kommentare auf %s',
+  'interactions:settings:enable_url_preview' => 'Aktiviere die URL-Vorschau',
+  'interactions:settings:enable_attachments:help' => 'Erlaube Dateien in Kommentaren',
+  'interactions:settings:enable_attachments' => 'Erlaube Dateien',
+  'interactions:settings:comment_form_position:before' => 'Vor der Liste',
+  'interactions:settings:comment_form_position:after' => 'Nach der Liste',
+  'interactions:settings:comments_order' => 'Reihenfolge der Kommentare',
+  'interactions:settings:comments_order:help' => 'In welcher Reihenfolge sollen die Kommentare angezeigt werden',
+  'interactions:settings:comments_order:chronological' => 'Chronologisch',
+  'interactions:settings:comments_order:reverse_chronological' => 'Umgekehrt Chronologisch',
+  'interactions:settings:comments_load_style' => 'Anzeigen und Laden der Kommentare',
+  'interactions:settings:comments_load_style:load_older' => 'Zeige neueste Kommentare mit einem Link um ältere zu laden',
+  'interactions:settings:comments_load_style:load_newer' => 'Zeige ältere Kommentare mit einem Link um neuere zu laden',
+  'interactions:settings:comments_load_limit' => 'Anzahl der anzuzeigenden Kommentare in der vollen Liste',
+  'interactions:settings:comments_visual_editor' => 'Aktiviere den visuellen Editor standardmäßig',
+  'interactions:settings:default_expand' => 'Erweitere den Kommentar-Tab',
+  'interactions:settings:comment_sort' => 'Erlaube Suchen und Filtern',
+  'interactions:object:hjwall' => 'Pinnwand-Eintrag',
+  'interactions:settings:comment_form_position' => 'Position des Kommentarfelds',
+  'interactions:likes:notifications:body' => '%s gefällt %s
+
+Hier kannst du den originalen Eintrag sehen:
+%s
+
+oder schaue in %ss Profil:
+%s',
+  'interactions:post' => 'Eintrag',
+  'interactions:comment:reply_to' => 'auf %s',
+  'interactions:comment:in_thread' => 'in %s',
+  'interactions:ownership:own' => 'ihrer %s',
+  'interactions:ownership:your' => 'deiner %s',
+  'interactions:ownership:owner' => '%ss %s',
+  'interactions:object:discussion' => 'Diskussionsthema',
+  'interactions:object:discussion_reply' => 'Diskussionsantwort',
+  'interactions:object:blog' => 'Blogeintrag',
+  'interactions:object:bookmarks' => 'Lesezeichen',
+);
+
+add_translation("de",$de_extend);
