c937385fef057783021fdf8b711aa94e669c2846
SteppoFF <steppo@sehr-chaos-company.de>
2017-06-11 12:27:20 +0200

    Create de.hypeWall.php

---
+++ languages/de.hypeWall.php
@@ -0,0 +1,104 @@
+<?php
+/*
+ * Translation for hypeWall
+ * @package hypeWall
+ * @subpackage Languages.German
+ */
+$de_extend = array(
+  'item:object:hjwall' => 'Pinnwandeinträge',
+  'wall' => 'Pinnwand',
+  'wall:settings:url' => 'Teile einen Link',
+  'wall:settings:photo' => 'Teile ein Foto',
+  'wall:settings:file' => 'Teile eine Datei',
+  'wall:settings:content' => 'Teile Inhalt',
+  'wall:view' => 'Zeige Pinnwand',
+  'wall:write' => 'Schreibe auf die Pinnwand',
+  'wall:empty' => 'Diese Pinnwand ist leer',
+  'wall:notfound' => 'Der Eintrag konnte nicht gefunden werden',
+  'wall:tag:friends' => 'Markiere Freunde',
+  'wall:tag:friends:hint' => 'Markiere Freunde: Beginne die Suche indem du den Namen schreibst',
+  'wall:tag:location:hint' => 'Verknüpfe einen Ort mit deinem Eintrag',
+  'wall:tag:location:findme' => 'Finde mich - Dein Browser könnte dich fragen, ob diese Seite deine momentante Position erfahren darf',
+  'wall:tag:river' => '%s hat %s in einem %s markiert',
+  'wall:tag:river:post' => 'Eintrag',
+  'wall:status:placeholder' => 'Was gibt es neues?',
+  'wall:tagged:post' => 'ein Eintrag',
+  'wall:url:placeholder' => 'Füge einen Link hinzu',
+  'wall:tagged:notification:subject' => '%s hat dich markiert in %s',
+  'wall:tagged:notification:message' => '%s hat dich in einem Eintrag markiert: <br>
+<blockquote><div>
+%s
+</div></blockquote>
+Du kannst den Eintrag hier sehen:
+%s',
+  'wall:new:notification:generic' => 'Neuer Eintrag',
+  'wall:new:notification:summary' => 'Neuer Eintrag %s',
+  'wall:new:notification:subject' => '%s veröffentlichte %s',
+  'wall:new:notification:message' => '%s veröffentlichte %s: <br>
+<blockquote><div>
+%s
+</div></blockquote>
+Du kannst den Eintrag hier sehen:
+%s',
+  'wall:owner:suffix' => 'auf %ss Pinnwand',
+  'wall:byline' => 'von %s',
+  'wall:with' => '- mit %s',
+  'wall:at' => 'in der Nähe von %s',
+  'wall:attached:single' => '[1 Anhang]',
+  'wall:attached' => '[%s Anhänge]',
+  'wall:new:wall:post' => '%s hat einen Eintrag auf %ss Pinnwand verfasst',
+  'wall:new:address' => 'hat einen Link geteilt',
+  'wall:new:images' => 'hat %s Bild(er) veröffentlicht',
+  'wall:new:items' => 'hat %s Dateien veröffentlicht',
+  'wall:new:attachments' => 'hat %s Bild(er) und %s andere Dateien veröffentlicht',
+  'wall:new:status' => 'veröffentlicht',
+  'wall:new:onwall' => 'auf der Pinwand von %s',
+  'hypeWall:usersettings:title' => 'Pinnwand',
+  'wall:widget:description' => 'Erlaube Benutzern deine Pinnwand zu sehen und Einträge zu verfassen',
+  'wall:numbertodisplay' => 'Anzahl der anzuzeigenden Einträge',
+  'wall:target:hjwall' => 'Pinnwand',
+  'wall:ownership:own' => 'auf ihrer %s',
+  'wall:ownership:your' => 'auf deiner %s',
+  'wall:ownership:owner' => 'auf %ss %s',
+  'wall:widget:showaddform' => 'Zeige',
+  'wall:groups' => 'Gruppen-Pinnwand',
+  'wall:groups:post' => 'Eintragen',
+  'wall:groups:enable' => 'Gruppen-Pinnwand aktivieren',
+  'wall:characters_remaining' => 'Zeichen verfügbar',
+  'wall:make_bookmark' => 'Speichere diesen Link in meinem Lesezeichen',
+  'wall:upload:error' => 'Datei konnte nicht hochgeladen werden',
+  'wall:upload:success' => 'Datei erfolgreich hochgeladen',
+  'wall:photo:placeholder' => 'Erzähle was über dieses Foto',
+  'wall:file:placeholder' => 'Erzähle was über diese Datei',
+  'wall:filehasntuploaded' => 'Bitte warte, bis die Datei hochgeladen wurde',
+  'wall:create:success' => 'Pinnwandeintrag erfolgreich gespeichert',
+  'wall:create:error' => 'Pinnwandeintrag konnte nicht gespeichert werden',
+  'wall:process:posting' => 'Speichern...',
+  'wall:error:not_found' => 'Eintrag nicht gefunden',
+  'wall:error:container_permissions' => 'Du hast nicht die nötigen Rechte um hier zu veröffentlichen',
+  'wall:edit' => 'Eintrag bearbeiten',
+  'wall:delete' => 'Eintrag löschen',
+  'wall:delete:success' => 'Eintrag wurde erfolgreich gelöscht',
+  'wall:delete:error' => 'Eintrag konnte nicht gelöscht werden',
+  'wall:post' => 'Veröffentlichen',
+  'wall:photo' => 'Teile ein Foto',
+  'wall:find_me' => 'Orte mich',
+  'wall:upload_file' => 'Wähle eine Datei',
+  'wall:tag_friends' => 'Markiere einen Freund',
+  'wall:location' => 'Füge einen Ort hinzu',
+  'wall:attachment' => 'Datei hinzufügen',
+  'wall:url' => 'Teile einen Link',
+  'wall:status' => 'Status aktualisieren',
+  'wall:usersettings:third_party_wall' => 'Erlaube Leuten, die nicht deine Freunde sind, auf deiner Pinnwand zu schreiben',
+  'wall:usersettings:river_access_id:help' => 'Wer kann sehen, wenn du auf eine fremden Pinnwand erwähnt wurdest, auch wenn der originale Beitrag nicht mit ihnen geteilt wurde?',
+  'wall:usersettings:river_access_id' => 'Sichtbarkeit von Pinnwand-Erwähnungen',
+  'wall:file' => 'Teile eine Datei',
+  'wall:owner' => 'Pinnwand von %s',
+  'wall:filefolder' => 'Pinnwand Uploads',
+  'wall:moreposts' => 'Mehr Einträge',
+  'wall:repost' => 'Veröffentliche auf deiner Pinnwand',
+  'wall:error:ajax' => 'Die angefragte Seite ist nicht erreichbar',
+  'wall:post:status_update' => 'Aktualisierten Status %s',
+);
+
+add_translation("de",$de_extend);
