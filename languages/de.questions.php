d747e6a30d829a2c3f9a349f38d4f125b5043e1b
SteppoFF <steppo@sehr-chaos-company.de>
2017-06-11 12:31:26 +0200

    Create de.questions.php

---
+++ languages/de.questions.php
@@ -0,0 +1,147 @@
+<?php
+/*
+ * Translation for questions
+ * @package questions
+ * @subpackage Languages.German
+ */
+$de_extend = array(
+  'answers' => 'Antworten',
+  'answers:addyours' => 'Füge eine Antwort hinzu',
+  'item:object:answer' => 'Antworten',
+  'item:object:question' => 'Fragen',
+  'questions:menu:user_hover:make_expert' => 'Mache zum Experten',
+  'questions:menu:user_hover:remove_expert' => 'Entferne Experte',
+  'questions:menu:user_hover:make_expert:confirm' => 'Bist du dir sicher, dass du diesen Nutzer zum Experten für %s machen willst?',
+  'questions:menu:user_hover:remove_expert:confirm' => 'Bist du dir sicher, dass du diesen Nutzer als Experten für %s entfernen möchtest?',
+  'questions:menu:entity:answer:mark' => 'Das ist richtig',
+  'questions:menu:entity:answer:unmark' => 'Nicht mehr richtig',
+  'questions:menu:filter:todo' => 'Offen',
+  'questions:menu:filter:todo_group' => 'Offen (Gruppe)',
+  'questions:menu:filter:experts' => 'Experten',
+  'river:create:object:question' => '%s stellte Frage %s',
+  'questions' => 'Fragen',
+  'questions:asked' => 'Gestellt von %s',
+  'river:create:object:answer' => '%s hat die frage %s beantwortet',
+  'questions:answered' => 'Zuletzt beantwortet von %s %s',
+  'questions:answered:correct' => 'Dir richtige Antwort kommt von %s %s',
+  'questions:everyone' => 'Alle Frage',
+  'questions:add' => 'Stelle eine Frage',
+  'questions:todo' => 'Todo',
+  'questions:todo:none' => 'Es gibt nicht zu tun, mache weiter so!',
+  'questions:owner' => 'Frage von %s',
+  'questions:none' => 'Es wurden noch keine Fragen gestellt.',
+  'questions:group' => 'Gruppiere Fragen',
+  'questions:enable' => 'Aktiviere das Gruppieren von Fragen',
+  'questions:edit:question:title' => 'Frage',
+  'questions:edit:question:description' => 'Details',
+  'questions:edit:question:container' => 'In welche Liste soll die Frage aufgenommen werden',
+  'questions:edit:question:container:select' => 'Bitte wähle eine Gruppe',
+  'questions:edit:question:move_to_discussions' => 'Verschiebe zu den Diskussionen',
+  'questions:edit:question:move_to_discussions:confirm' => 'Bist du dir sicher, dass du diese Frage zu den Diskussionen verschieben willst? Das kann nicht rückgängig gemacht werden! !',
+  'questions:object:answer:title' => 'Antwort auf die Frage %s',
+  'questions:experts:title' => 'Experten',
+  'questions:experts:none' => '%s wurde noch keinem Experten zugewiesen.',
+  'questions:experts:description:group' => 'Nachfolgend findest du eine Liste von Experten für %s. Diese Leute werden dir die Fragen beantworten.',
+  'questions:notifications:create:subject' => 'Es wurde eine neue Frage gestellt',
+  'questions:notifications:create:summary' => 'Es wurde eine neue Frage gestellt',
+  'questions:notifications:create:message' => 'Hallo %s
+
+Die folgende Frage wurde gestellt: %s
+
+Um die Frage zu beantworten:
+%s',
+  'questions:notifications:move:subject' => 'Eine Frage wurde verschoben',
+  'questions:notifications:move:summary' => 'Eine Frage wurde verschoben',
+  'questions:notifications:move:message' => 'Hallo %s
+
+Die Frage: %s wurde verschoben.
+Du musst sie somit nicht länger beantworten.
+
+Um die Frage dennoch zu beantworten:
+%s',
+  'questions:notifications:answer:create:subject' => 'Es wurde auf folgende Frage geantwortet: %s',
+  'questions:notifications:answer:create:summary' => 'Es wurde auf folgende Frage geantwortet: %s',
+  'questions:notifications:answer:create:message' => 'Hallo %s
+
+%s hat die Frage %s beantwortet.
+
+%s
+
+Um zur Antwort zu gelangen, klicke bitte hier:
+%s',
+  'questions:notifications:answer:correct:subject' => 'Eine Antwort wurde as richtig markiert am %s',
+  'questions:notifications:answer:correct:summary' => 'Eine Antwort wurde as richtig markiert am %s',
+  'questions:notifications:answer:comment:subject' => 'Neuer Kommentar zu einer Antwort',
+  'questions:notifications:answer:comment:summary' => 'Neuer Kommentar zu einer Antwort',
+  'questions:notifications:answer:correct:message' => 'Hallo %s
+
+%s hat die Antwort auf die Frage %s als richtig markiert:
+
+%s
+
+Um zur Antwort zu gelangen, klicke bitte hier:
+%s',
+  'questions:daily:notification:message:more' => 'Mehr zeigen',
+  'questions:daily:notification:message:overdue' => 'Die folgenden Fragen sind überfällig',
+  'questions:daily:notification:message:due' => 'Die folgenden Fragen müssen heute beantwortet werden',
+  'questions:daily:notification:message:new' => 'Neue Frage gestellt',
+  'questions:answer:edit' => 'Antwort aktualisieren',
+  'questions:search:answer:title' => 'Antwort',
+  'questions:settings:general:title' => 'Allg. Einstellungen',
+  'questions:settings:general:close' => 'Schließe eine Frage, wenn eine richtige Antwort vorhanden ist',
+  'questions:group_settings:who_can_ask:experts' => 'Nur Experten',
+  'questions:group_settings:who_can_ask:members' => 'Alle Mitglieder',
+  'questions:group_settings:who_can_ask' => 'Wer kann in dieser Gruppe Fragen stellen?',
+  'questions:group_settings:solution_time:description' => 'Fragen sollten innerhalb dieses Zeitrahmens beantwortet werden. 0 für kein Zeitlimit.',
+  'questions:group_settings:who_can_answer' => 'Wer kann Fragen in dieser Gruppe beantworten',
+  'questions:group_settings:who_can_answer:experts_only' => 'Der Administrator hat eingestellt, dass nur Experten auf Fragen antworten können',
+  'questions:group_settings:auto_mark_correct' => 'Wenn ein Experte antwortet, markiere die Antwort automatisch als richtig.',
+  'widget:questions:title' => 'Fragen',
+  'widget:questions:description' => 'Du kannst den Status deiner Fragen einsehen.',
+  'widget:questions:content_type' => 'Welche Fragen sollen angezeigt werden',
+  'widget:questions:more' => 'Zeite weitere Fragen',
+  'questions:action:answer:save:error:container' => 'Du hast nicht die nötigen Rechte, um diese Frage zu beantworten!',
+  'questions:action:answer:save:error:save' => 'Beim Speichern deiner Antwort ist ein Problem aufgetreten!',
+  'questions:action:answer:save:error:question_closed' => 'Die Frage, die du beantworten möchtest, ist bereits geschlossen',
+  'questions:action:answer:toggle_mark:error:not_allowed' => 'Du hast nicht die nötigen Rechte, um eine Antwort als richtig zu markieren',
+  'questions:action:answer:toggle_mark:error:duplicate' => 'Es gibt bereits eine richtige Antwort auf diese Frage',
+  'questions:action:answer:toggle_mark:success:mark' => 'Die Antwort ist als richtige Antwort markiert',
+  'questions:action:answer:toggle_mark:success:unmark' => 'Die Antwort ist nicht länger als richtige Antwort markiert',
+  'questions:action:question:save:error:container' => 'Du hast nicht die nötigen Rechte, um hier eine Frage zu stellen',
+  'questions:action:question:save:error:save' => 'Beim Speichern deine Frage ist ein Problem aufgetreten',
+  'questions:action:question:save:error:body' => 'Ein Titel und eine Beschreibung sind zwingend erforderlich: %s, %s',
+  'questions:settings:general:close:description' => 'When eine Antwort als korrekt markiert wurde, schließe die Frage. Das bedeutet, dass keine weiteren Antworten gegeben werden können!',
+  'questions:settings:experts:answer' => 'Nur Experten können Fragen beantworten',
+  'questions:settings:experts:mark' => 'Nur Experten können Anworten als richtig markieren',
+  'questions:settings:access:title' => 'Zugriffsbeschränkungen',
+  'questions:settings:access:options:user' => 'Benutzerdefiniert',
+  'questions:settings:access:options:group' => 'Gruppenmitglieder',
+  'questions:group_settings:title' => 'Frageeinstellungen',
+  'questions:action:group_settings:success' => 'Die Gruppeneinstellungen wurden gespeichert',
+  'questions:action:question:move_to_discussions:success' => 'Die Frage wurde zu einer Diskussion verschoben',
+  'questions:action:question:move_to_discussions:error:topic' => 'Beim Erstellen eines Diskussionsthemas ist ein Fehler aufgetreten, versuche es erneut',
+  'questions:action:question:move_to_discussions:error:move' => 'Du hast nicht die nötigen Rechte, um eine Frage zu den Diskussionen zu verschieben',
+  'questions:action:toggle_expert:success:make' => '%s ist nun ein Experte für %s',
+  'questions:action:toggle_expert:success:remove' => '%s ist nicht länger ein Experte für %s',
+  'questions:answer:checkmark:title' => '%s hat diese Antwort als richtig markiert am %s',
+  'questions:settings:general:solution_time' => 'Setze eine Standard-Beantwortungszeit in Tagen',
+  'questions:notifications:answer:comment:message' => 'Hallo %s
+
+%s hat die folgende Frage kommentiert \'%s\'.
+
+%s
+
+Um den Kommentar zu sehen, klicke bitte hier:
+%s',
+  'questions:action:answer:save:error:body' => 'Ein Inhalt ist erforderlich: %s, %s',
+  'questions:action:question:save:error:limited_to_groups' => 'Fragen sind nur in Gruppen erlaubt, bitte wähle eine Gruppe',
+  'questions:settings:experts:title' => 'Q&A Einstellungen für Experten',
+  'questions:settings:experts:enable' => 'Aktiviere die Rolle von Experten',
+  'questions:settings:experts:enable:description' => 'Experten haben besondere Rechte und können von Administratoren oder Gruppenbesitzern ernannt werden',
+  'questions:settings:access:personal' => 'Was is das Zutrittslevel für persönliche Fragen',
+  'questions:settings:access:group' => 'Was is das Zutrittslevel für Fragen in Gruppen',
+  'questions:settings:general:limit_to_groups' => 'Gestatte Fragen nur im Zusammenhang mit Gruppen',
+  'questions:settings:general:limit_to_groups:description' => 'Falls das aktiviert wird, können fragen nicht mehr im persönlichen Kontext gestellt werden',
+);
+
+add_translation("de",$de_extend);
