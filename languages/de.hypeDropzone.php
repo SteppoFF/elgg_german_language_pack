2d4ceb806b78325a1cf6f1b1479ec758f3c3a814
SteppoFF <steppo@sehr-chaos-company.de>
2017-06-11 12:17:00 +0200

    Create de.hypeDropzone.php

---
+++ languages/de.hypeDropzone.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * Translation for hypeDropzone
+ * @package hypeDropzone
+ * @subpackage Languages.German
+ */
+$de_extend = array(
+  'dropzone:response_error' => 'Es ist ein Fehler aufgetreten',
+  'dropzone:default_message' => '<strong>Lege deine Dateien hier ab</strong><br><span>oder klicke um eine Datei von deinem Gerät auszuwählen</span>',
+  'dropzone:remove_file' => 'Entfernen',
+  'dropzone:max_files_exceeded' => 'Du hast die maximale Anzahl an Dateien erreicht, die du gleichzeitig hochladen kannst',
+  'dropzone:fallback_message' => 'Dein Browser unterstützt kein Hochladen per "Drag and Drop"',
+  'dropzone:fallback_text' => 'Bitte benutze die Dateieingabe um klassisch hochzuladen',
+  'dropzone:invalid_filetype' => 'Dieser Dateityp ist nicht gestattet',
+  'dropzone:file_too_big' => 'Die Datei ist zu groß',
+  'dropzone:cancel_upload' => 'Hochladen abbrechen',
+  'dropzone:cancel_upload_confirmation' => 'Bist du sicher, dass du das Hochladen abbrechen möchtest?',
+  'dropzone:server_side_error' => 'Server Fehler',
+  'dropzone:file_not_entity' => 'Datei konnte nicht erstellt werden',
+);
+
+add_translation("de",$de_extend);
