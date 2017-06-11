d382e2d1a095bdb3b084bfd615e3f8cc8e7c8090
SteppoFF <steppo@sehr-chaos-company.de>
2017-06-11 12:28:24 +0200

    Create de.powered_by_elgg.php

---
+++ languages/de.powered_by_elgg.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * Translation for powered_by_elgg
+ * @package powered_by_elgg
+ * @subpackage Languages.German
+ */
+$de_extend = array(
+  'settings:footer:config' => 'Footer Konfiguration',
+  'settings:footer:name' => 'Gib deinem Footer-Element einen Namen',
+  'settings:footer:link' => 'Füge einen Link für das Footer-Element hinzu',
+  );
+
+add_translation("de",$de_extend);
