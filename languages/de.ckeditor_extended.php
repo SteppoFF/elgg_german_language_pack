3c4bfa3c0308760a9e5ebddaf2052548cc166dfc
SteppoFF <steppo@sehr-chaos-company.de>
2017-06-11 12:36:03 +0200

    Create de.ckeditor_extended.php

---
+++ languages/de.ckeditor_extended.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * Translation for ckeditor_extended
+ * @package ckeditor_extended
+ * @subpackage Languages.German
+ */
+$de_extend = array(
+  'ckeditor_extended:settings:image_upload_allowed' => 'Gestatte das Hochladen von Bildern durch den Editor',
+  'ckeditor_extended:settings:image_upload_browse' => 'Gestatte das Durchsuchen von zuvor hochgeladenen Bildern',
+  'ckeditor_extended:settings:overwrite_uploaded_images' => 'Überschreibe beim Hochladen bestehende Bilder mit dem gleichen Namen',
+  'ckeditor_extended:settings:show_html_toggler:option:admin_only' => 'Nur für Administratoren',
+  'ckeditor_extended:browse:files:user' => 'Deine Dateien',
+  'ckeditor_extended:browse:files:site' => 'Daten der Seite',
+  'ckeditor_extended:output:inline_edit:default' => 'Bisher wurde kein Inhalt gespeichert. Fange hier an zu schreiben',
+  'ckeditor_extended:upload:resize_failed' => 'Das Bild konnte nicht angepasst werden',
+  'ckeditor_extended:upload:no_upload' => 'Kein hochgeladene Datei gefunden. Vielleicht ist die Datei zu groß',
+  'ckeditor_extended:upload:no_image' => 'Die Datei ist kein Bild',
+  'ckeditor_extended:settings:example' => 'Beispiel deiner Enstellungen',
+  'ckeditor_extended:settings:htmlawed:schemes' => 'Schemata',
+  'ckeditor_extended:settings:htmlawed:elements' => 'HTML Elemente',
+  'ckeditor_extended:settings:editor_config' => 'Bitte gib hier die neuen Einstellungen ein',
+  'ckeditor_extended:settings:link' => 'Besuche die Webseite des CKEditors für alle verfügbaren Einstellungen',
+  'ckeditor_extended:settings:show_html_toggler' => 'Zeige den HTML-Umschalter-Link',
+  'ckeditor_extended:settings:htmlawed:title' => 'HTMLawed Einstellungen',
+  'ckeditor_extended:settings:htmlawed:info' => 'Konfiguriere extra Regeln für HTMLawed um bestimmten Elementen oder Schemata zu vertrauen',
+);
+
+add_translation("de",$de_extend);
