2171a12f48a207c7fef665e72e1838965cede02f
SteppoFF <steppo@sehr-chaos-company.de>
2017-06-11 12:20:29 +0200

    Create de.hypeEmbed.php

---
+++ languages/de.hypeEmbed.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * Translation for hypeEmbed
+ * @package hypeEmbed
+ * @subpackage Languages.German
+ */
+$de_extend = array(
+  'embed:player:address' => 'URL des Inhaltes',
+  'sort:object:filter:simpletype:all' => 'Alle typen',
+  'sort:object:filter:simpletype:image' => 'Bilder',
+  'sort:object:filter:simpletype:general' => 'Andere',
+  'sort:object:filter:simpletype:document' => 'Dokumente',
+  'sort:object:filter:simpletype:video' => 'Videos',
+  'sort:object:filter:simpletype:audio' => 'Audio',
+  'item:object:embed_file' => 'Eingebettete Dateien',
+  'embed:upload_type:file' => 'Füge diese Datei deinen Dateien hinzu and mich sie suchbar',
+  'embed:upload_type:embed_file' => 'Benutze diese Datei nur für das Einbetten in Dokumente',
+  'embed:tab:file:empty' => 'Es wurden bisher keine Dateien hochgeladen',
+  'embed:tab:posts:empty' => 'Es wurden bisher keine Dateien veröffentlicht',
+  'embed:assets' => 'Assets',
+  'embed:player' => 'Player',
+  'embed:ecml:error' => 'Eingebetteter Inhalt konnte nicht geladen werden. Er wurde entfernt oder du hast nicht die nötigen Rechte um den Inhalt zu sehen',
+  'embed:upload_type' => 'Dateityp:',
+);
+
+add_translation("de",$de_extend);
