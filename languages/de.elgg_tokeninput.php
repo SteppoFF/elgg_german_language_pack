a210bb886520a1fc3196ba14f00eb464b3b465a2
SteppoFF <steppo@sehr-chaos-company.de>
2017-06-11 12:37:47 +0200

    Create de.elgg_tokeninput.php

---
+++ languages/de.elgg_tokeninput.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Translation for elgg_tokeninput
+ * @package elgg_tokeninput
+ * @subpackage Languages.German
+ */
+$de_extend = array(
+  'tokeninput:text:placeholder' => 'Bitte wählen..',
+  'tokeninput:text:hint' => 'Schreibe etwas, um die Suche zu starten',
+  'tokeninput:text:noresults' => 'Keine Ergebnisse',
+  'tokeninput:text:searching' => 'Suche...',
+  'tokeninput:text:delete' => '×',
+  'tokeninput:suggest' => 'Füge "<b>%s</b>" hinzu',
+);
+
+add_translation("de",$de_extend);
