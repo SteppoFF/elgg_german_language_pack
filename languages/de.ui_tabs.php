16619e7332ab2c6869f343bd496dff58e078b1a3
SteppoFF <steppo@sehr-chaos-company.de>
2017-06-11 12:33:19 +0200

    Create de.ui_tabs.php

---
+++ languages/de.ui_tabs.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Translation for ui_tabs
+ * @package ui_tabs
+ * @subpackage Languages.German
+ */
+$de_extend = array(
+  'ui:tabs:settings:ajax_page_tabs' => 'AJAX-Unterstützung für alle Tabs im "Content"-Layout',
+);
+
+add_translation("de",$de_extend);
