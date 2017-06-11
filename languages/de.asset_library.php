ff90179c95a55cd294f989083b47915021a269ae
SteppoFF <steppo@sehr-chaos-company.de>
2017-06-11 12:07:45 +0200

    Create de.asset_library.php

---
+++ languages/de.asset_library.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * Translation for asset_library
+ * @package asset_library
+ * @subpackage Languages.German
+ */
+$de_extend = array(
+  'asset_library:admin:assets:list' => 'Dateien',
+  'asset_library:forms:upload:file:label' => 'Wähle eine Datei zum Hochladen',
+  'asset_library' => 'Asset Bibliothek',
+  'admin:administer_utilities:assets' => 'Assets verwalten',
+  'asset_library:admin:info' => 'Lade Asses hoch, die dann (öffentlich) zur Nutzung auf der Seite verfügbar sind. Kopiere den Link zur Datei und benutze ihn überall.',
+);
+
+add_translation("de",$de_extend);
