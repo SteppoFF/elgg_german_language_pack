f2e3ff9e2b5bc63d8c525f46c9ba49894ff516b4
SteppoFF <steppo@sehr-chaos-company.de>
2017-06-11 12:21:31 +0200

    Create de.hypeInbox.php

---
+++ languages/de.hypeInbox.php
@@ -0,0 +1,123 @@
+<?php
+/*
+ * Translation for hypeInbox
+ * @package hypeInbox
+ * @subpackage Languages.German
+ */
+$de_extend = array(
+  'admin:inbox' => 'Nachrichten Einstellungen',
+  'admin:inbox:message_types' => 'Nachrichten Typen',
+  'item:object:message:create' => 'Erstelle einen neuen Nachrichtentyp',
+  'item:object:message:name' => 'Eindeutiger Typname',
+  'item:object:message:setting:multiple' => 'Erlaube mehrere Empfänger',
+  'item:object:message:setting:attachments' => 'Erlaube Dateianhänge',
+  'item:object:message:setting:persistent' => 'Schreibgeschützt (Kann vom Empfänger nicht gelöscht werden)',
+  'item:object:message:setting:no_subject' => 'Deaktiviere "Betreff"',
+  'item:object:message:all' => 'Alle Nachrichten',
+  'inbox:admin:import' => 'Alte Nachrichten importieren',
+  'inbox:admin:import_stats' => '%s Nachrichten fehlen Metadaten, die zur Funktion von hypeInbox benötigt werden',
+  'inbox:admin:import_start' => 'Starte Import',
+  'inbox:admin:import_complete' => 'Import abgeschlossen',
+  'inbox' => 'Nachrichten',
+  'inbox:recipient' => 'Empfänger',
+  'inbox:user_type:admin' => 'Administrator',
+  'inbox:user_type:all' => 'Beliebiger Benutzer',
+  'inbox:user_type:editor' => 'Editor',
+  'inbox:user_type:supervisor' => 'Supervisor',
+  'inbox:user_type:observer' => 'Beobachter',
+  'inbox:send' => 'Schicke %s',
+  'inbox:sender' => 'Absender',
+  'inbox:relationship' => 'Verhältnis zum Empfänger',
+  'inbox:all' => 'Alle Nachrichten',
+  'inbox:inbox' => 'Posteingang',
+  'inbox:sent' => 'Postausgang',
+  'inbox:reply' => 'Antworten',
+  'inbox:reply:message_type' => 'Eine eine %s Antworten',
+  'inbox:reply:prefix' => 'Aw:',
+  'inbox:message_type' => '%s',
+  'inbox:conversation:user' => 'Unterhaltung mit %s',
+  'inbox:conversation:group' => 'Gruppenunterhaltung',
+  'inbox:usersettings:grouping' => 'Gruppiere den Posteingang nach Absender',
+  'inbox:group' => 'Gruppiere',
+  'inbox:dontgroup' => 'Nicht Gruppieren',
+  'inbox:message_not_found' => 'Nachricht nicht gefunden',
+  'inbox:me' => 'mich',
+  'inbox:recipients:others' => '%s andere',
+  'inbox:untitled' => 'Ohne Titel',
+  'inbox:thread:new' => 'Neue Nachricht',
+  'inbox:attachments:count' => '%s Anlagen',
+  'inbox:message' => 'Nachricht: %s',
+  'inbox:thread:unread' => '%s neue',
+  'inbox:thread:count' => '%s Nachrichten',
+  'inbox:thread' => 'Zeige alle %s Nachrichten in diesem Thema',
+  'item:object:message:__private:plural' => 'Private Nachrichten',
+  'item:object:message:__private:singular' => 'Private Nachricht',
+  'inbox:search' => 'Suche',
+  'messages:forward:byline' => 'Weitergeleitet Nachricht von %s (ursprünglich gesendet am %s)',
+  'inbox:byline:thread' => 'Letzte Nachricht von %s',
+  'inbox:to' => 'An %s',
+  'inbox:byline' => 'Von %s',
+  'inbox:settings:enable_html' => 'Aktiviere HTML in Nachrichten',
+  'inbox:empty' => 'Keine Nachrichten im Posteingang',
+  'inbox:settings:enable_html:help' => 'HTML Nachrichten sollten nur eingeschaltet werden, wenn deine Seite den Versand von HTML E-Mails unterstützt',
+  'inbox:notification:subject' => 'Du hast eine neue %s',
+  'inbox:message:send' => 'Abschicken',
+  'inbox:message:attachments:add' => 'Anhänge hinzufügen',
+  'inbox:message:attachments' => 'Anhänge',
+  'inbox:message:body' => 'Nachricht',
+  'inbox:message:subject' => 'Betreff',
+  'inbox:message:recipients' => 'Empfänger',
+  'inbox:message:recipient' => 'Empfänger',
+  'inbox:form:toggle_all' => 'Alle auswählen',
+  'inbox:user:unknown' => 'Unbekannt',
+  'inbox:send:error:generic' => 'Beim Versand der Nachricht ist ein unbekannter Fehler aufgetreten',
+  'inbox:delete' => 'Löschen',
+  'inbox:forward' => 'Weiterleiten',
+  'inbox:markread' => 'Als gelesen markieren',
+  'inbox:markunread' => 'Als ungelesen markieren',
+  'inbox:delete:success' => '%s Nachrichten wurden erfolgreich gelöscht',
+  'inbox:delete:success:single' => 'Nachricht wurde erfolgreich gelöscht',
+  'inbox:delete:error' => 'Nachricht konnte nicht gelöscht werden',
+  'inbox:delete:inbox:confirm' => 'Bist du sicher, dass du alle ausgewählten Nachrichten löschen willst?',
+  'inbox:delete:thread:confirm' => 'Bist du sicher, dass du alle Nachrichten dieser Unterhaltung löschen möchtest?',
+  'inbox:delete:message:confirm' => 'Bist du sicher, dass du diese Nachricht löschen möchtest?',
+  'inbox:markread:success' => '%s Nachrichten wurden als gelesen markiert',
+  'inbox:markread:success:single' => 'Nachricht wurde als gelesen markiert',
+  'inbox:markread:error' => 'Nachricht konnte nicht als gelesen markiert werden',
+  'inbox:markunread:success' => '%s Nachrichten wurden als ungelesen markiert',
+  'inbox:markunread:success:single' => 'Nachricht wurde als ungelesen markiert',
+  'inbox:markunread:error' => 'Nachricht konnte nicht als ungelesen markiert werden',
+  'inbox:error:notfound' => '%s Nachrichten konnten nicht gefunden werden',
+  'inbox:send:success' => 'Nachricht erfolgreich versandt',
+  'inbox:send:error:no_recipients' => 'Es wurden keine Empfänger gewählt',
+  'inbox:send:error:no_body' => 'Du brauchst einen Inhalt für deine Nachricht',
+  'inbox:load:after' => 'Lade neuere Nachrichten',
+  'inbox:load:before' => 'Lade frühere Nachrichten',
+  'inbox:conversation' => 'Unterhaltung zwischen dir und %s',
+  'inbox:new' => 'Neue %s',
+  'inbox:compose' => 'Verfassen',
+  'inbox:compose:message_type' => 'Verfasse eine %s',
+  'inbox:compose:forward' => 'Leite eine %s weiter',
+  'inbox:message_type:sent' => 'Gesendete',
+  'inbox:thread:participants' => 'Teilnehmer in dieser Unterhaltung',
+  'inbox:nomessages' => 'In dieser Unterhaltung sind keine Nachrichten',
+  'inbox:error:persistent' => '%s Nachrichten konnten nicht gelöscht werden, da sie den Status "nur lesen" haben',
+  'inbox:error:unknown' => '%s Nachrichten konnten wegen eines unbekannten Fehlers nicht gelöscht werden',
+  'inbox:notification:body' => 'Du hast eine neue %s von %s. Sie lautet:
+
+
+%s
+
+
+Um die Nachricht zu sehen, klicke bitte hier:
+
+%s
+
+Um %s eine Nachricht zu schicken, klicke bitter hier:
+
+%s
+
+Bitte antworte nicht auf diese E-Mail',
+);
+
+add_translation("de",$de_extend);
