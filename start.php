f6fc26ecbc357896f35c7db328bcbd5d561ae88c
SteppoFF <steppo@sehr-chaos-company.de>
2017-06-11 11:56:44 +0200

    Create start.php

---
+++ start.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * Yet another language pack for Elgg
+ *
+ * Just want to save all my translations. Feel free to use them!
+ *
+ * (C) SteppoFF 2017
+ * GNU Public License version 2
+ *
+ * https://github.com/SteppoFF
+ */
+register_translations(dirname(__FILE__) . "/languages/", true);
