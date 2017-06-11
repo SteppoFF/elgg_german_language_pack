878c60db5246fecb5509917d905a90e6374f52fd
SteppoFF <steppo@sehr-chaos-company.de>
2017-06-11 12:15:56 +0200

    Create de.hypeAttachments.php

---
+++ languages/de.hypeAttachments.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * Translation for hypeAttachments
+ * @package hypeAttachments
+ * @subpackage Languages.German
+ */
+$de_extend = array(
+  'attachments:upload' => 'Anhang hinzufügen',
+  'attachments:detach' => 'Löschen',
+  'attachments:detach:success' => 'Anhang wurde erfolgreich entfernt',
+  'attachments:detach:error' => 'Anhang konnte nicht entfernt werden',
+  'attachments:title' => 'Anhänge',
+  'attachments:count' => '%s Anhänge',
+  'attachments:files' => 'Wähle Dateien',
+  'attachments:attach' => 'Anhängen',
+  'attachments:attach:success' => '%2$s wurde angehängt an %1$s',
+  'attachments:attach:error' => 'Beim erstellen des Anhangs ist ein Fehler aufgetreten',
+  'attachments:no_results' => 'Diese Meldung hat keine Anhänge',
+  'attachments:upload:success' => '%s neue Anhänge wurden gemacht',
+  'attachments:notify:subject' => 'Neuer Anhang für %s',
+  'attachments:notify:summary' => 'Neue Anhänge für %s',
+  'attachments:notify:body' => '%s hat eine Anhänge für %s erstellt:
+
+%s
+
+Du kanst die Dateien hier finden:
+%s',
+  'attachments:upload:empty' => 'Es konnten keine Anhänge erstellt werden',
+  'attachments:settings:allow_attachments' => 'Wähle die Objekte, die Anhänge haben können',
+);
+
+add_translation("de",$de_extend);
